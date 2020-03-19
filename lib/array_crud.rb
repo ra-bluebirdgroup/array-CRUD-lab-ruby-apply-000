196047538d5d74b62633ce9e1f1edaeb402bc035
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-19 21:42:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ array << element
 end
 
 def add_element_to_start_of_array(array, element)
-array.unshift = element
+array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
@@ -27,9 +27,9 @@ array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-
+array[0]
 end
 
 def retrieve_last_element_from_array(array)
-
+array[-1]
 end
